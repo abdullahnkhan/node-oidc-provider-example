d63d6325e068d8df4fb0f4d1c4edaa25bc1c4e80
Abdullah Khan <akhan1@mac.com>
2019-03-28 10:11:37 -0700

    added a redis adapter for tokens and sessions

--- src/index.js
+++ src/index.js
@@ -14,9 +14,12 @@ assert(process.env.HEROKU_APP_NAME, 'process.env.HEROKU_APP_NAME missing');
 assert(process.env.PORT, 'process.env.PORT missing');
 assert(process.env.SECURE_KEY, 'process.env.SECURE_KEY missing, run `heroku addons:create securekey`');
 assert.equal(process.env.SECURE_KEY.split(',').length, 2, 'process.env.SECURE_KEY format invalid');
+assert(process.env.REDIS_URL, 'process.env.REDIS_URL missing, run `heroku-redis:hobby-dev`');
+
+// require the redis adapter factory/class
+const RedisAdapter = require('./redis_adapter');
 
 const oidc = new Provider(`https://${process.env.HEROKU_APP_NAME}.herokuapp.com`, {
-  // enable some of the feature, see the oidc-provider readme for more
   formats: {
     AccessToken: 'jwt',
   },
@@ -36,7 +39,18 @@ const keystore = require('./keystore.json');
 
 oidc.initialize({
   keystore,
-  clients: [{ client_id: 'foo', client_secret: 'bar', redirect_uris: ['http://lvh.me/cb'] }],
+  clients: [
+    // reconfigured the foo client for the purpose of showing the adapter working
+    {
+      client_id: 'foo',
+      redirect_uris: ['https://example.com'],
+      response_types: ['id_token token'],
+      grant_types: ['implicit'],
+      token_endpoint_auth_method: 'none',
+    },
+  ],
+  // configure Provider to use the adapter
+  adapter: RedisAdapter,
 }).then(() => {
   oidc.proxy = true;
   oidc.keys = process.env.SECURE_KEY.split(',');
